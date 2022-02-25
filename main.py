38275bbf0de1d7c5fd8c4d632ccfcc71e10fc2bd
Peter2021-YZU <98403788+Peter2021-YZU@users.noreply.github.com>
2022-02-26 01:08:16 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-def mutate_string(string, position, character):
-    return string[:position] + character + string[position+1:]
+# This is a sample Python script.
 
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def solve(s):
+    a = s.split()
+    a = [b[0].upper()+b[1:] for b in a]
+    return (" ".join(a))
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    s = input()
-    i, c = input().split()
-    s_new = mutate_string(s, int(i), c)
-    print(s_new)
+    print(solve('PyCharm is the best'))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
